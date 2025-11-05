6522ff691461be7f78d296c8730049aed69e2370
tenoriodiego <zediegot@gmail.com>
2025-11-05 17:46:45 -0300

    feat: spring-boot-io-initializr files

---
+++ src/main/java/br/com/precojusto/desafio_granja_patos/demo/DesafioGranjaPatosApplication.java
@@ -0,0 +1,13 @@
+package br.com.precojusto.desafio_granja_patos.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioGranjaPatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioGranjaPatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/precojusto/desafio_granja_patos/demo/DesafioGranjaPatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.precojusto.desafio_granja_patos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioGranjaPatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
